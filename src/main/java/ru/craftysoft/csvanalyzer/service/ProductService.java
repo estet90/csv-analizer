2805dbf2128b81197857d6ac80e14a756a1e55ec
dkononov <estetis21@gmail.com>
2020-06-09 09:41:33 +0400

    разбил основной алгоритм на несколько методов

--- src/main/java/ru/craftysoft/csvanalyzer/service/ProductService.java
+++ src/main/java/ru/craftysoft/csvanalyzer/service/ProductService.java
@@ -19,102 +19,118 @@ public class ProductService {
                 : Integer.compare(o1.hashCode(), o2.hashCode());
     });
     private final Map<Integer, TreeMap<Float, Product>> productsByIdGrouping = new HashMap<>();
+    private final int maxNumberOfProductsWithTheSameId = 20;
 
     public TreeSet<Product> getProducts() {
         return products;
     }
 
-    //возможно этот метод стоит разделить на несколько методов поменьше. но я не хочу иметь много методов, меняющих глобальное состояние.
     public synchronized void processProduct(Product product) {
         if (products.contains(product)) {
             return;
         }
         var productsLimit = 1000;
         if (products.size() == productsLimit) {
-            var price = product.price();
-            var lastProduct = products.last();
-            if (price >= lastProduct.price()) {
-                return;
-            }
-            var firstProduct = products.first();
-            var id = product.id();
-            if (price <= firstProduct.price()) {
-                var map = productsByIdGrouping.get(id);
-                if (nonNull(map)) {
-                    if (map.size() == 20) {
-                        var lastEntry = map.pollLastEntry();
-                        products.remove(lastEntry.getValue());
-                    } else {
-                        ofNullable(products.pollLast())
-                                .ifPresent(removedProduct -> ofNullable(productsByIdGrouping.get(removedProduct.id()))
-                                        .ifPresent(treeMap -> treeMap.remove(removedProduct.price()))
-                                );
-                    }
-                    map.put(price, product);
-                } else {
-                    ofNullable(products.pollLast())
-                            .ifPresent(removedProduct -> ofNullable(productsByIdGrouping.get(removedProduct.id()))
-                                    .ifPresent(treeMap -> treeMap.remove(removedProduct.price()))
-                            );
-                    productsByIdGrouping.put(id, new TreeMap<>(Map.of(price, product)));
-                }
-                products.add(product);
-            } else {
-                var map = productsByIdGrouping.get(id);
-                if (nonNull(map) && !(map.size() == 20 && price >= map.lastKey())) {
-                    for (var entry : map.entrySet()) {
-                        if (entry.getKey() > price) {
-                            if (map.size() == 20) {
-                                var lastEntry = map.pollLastEntry();
-                                products.remove(lastEntry.getValue());
-                            } else {
-                                ofNullable(products.pollLast())
-                                        .ifPresent(removedProduct -> ofNullable(productsByIdGrouping.get(removedProduct.id()))
-                                                .ifPresent(treeMap -> treeMap.remove(removedProduct.price()))
-                                        );
-                            }
-                            var size = map.size();
-                            map.put(price, product);
-                            if (size < map.size()) {
-                                products.add(product);
-                            }
-                            break;
-                        }
-                    }
-                } else {
-                    ofNullable(products.pollLast())
-                            .ifPresent(removedProduct -> ofNullable(productsByIdGrouping.get(removedProduct.id()))
-                                    .ifPresent(treeMap -> treeMap.remove(removedProduct.price()))
-                            );
-                    productsByIdGrouping.put(id, new TreeMap<>(Map.of(price, product)));
-                    products.add(product);
-                }
-            }
+            handleProductWhenProductsHasMaxSize(product);
         } else if (products.size() == 0) {
             products.add(product);
             productsByIdGrouping.put(product.id(), new TreeMap<>(Map.of(product.price(), product)));
         } else {
-            var price = product.price();
-            var last = products.last();
-            var id = product.id();
-            if (price >= last.price()) {
-                var map = productsByIdGrouping.get(id);
-                if (nonNull(map)) {
-                    if (map.size() < 20) {
-                        products.add(product);
-                        map.put(price, product);
-                    }
-                } else {
-                    products.add(product);
-                    productsByIdGrouping.put(id, new TreeMap<>(Map.of(price, product)));
-                }
-                return;
+            handleProduct(product);
+        }
+    }
+
+    private void handleProductWhenProductsHasMaxSize(Product product) {
+        var price = product.price();
+        var lastProduct = products.last();
+        if (price >= lastProduct.price()) {
+            return;
+        }
+        var firstProduct = products.first();
+        if (price <= firstProduct.price()) {
+            addProductToStartWhenProductsHasMaxSize(product);
+        } else {
+            addProductWhenProductsHasMaxSize(product);
+        }
+    }
+
+    private void addProductToStartWhenProductsHasMaxSize(Product product) {
+        var id = product.id();
+        var price = product.price();
+        var map = productsByIdGrouping.get(id);
+        if (nonNull(map)) {
+            if (map.size() == maxNumberOfProductsWithTheSameId) {
+                var lastEntry = map.pollLastEntry();
+                products.remove(lastEntry.getValue());
+            } else {
+                pollLastProduct();
             }
-            var first = products.first();
-            if (price <= first.price()) {
-                var map = productsByIdGrouping.get(id);
-                if (nonNull(map)) {
-                    if (map.size() == 20) {
+            map.put(price, product);
+        } else {
+            pollLastProduct();
+            productsByIdGrouping.put(id, new TreeMap<>(Map.of(price, product)));
+        }
+        products.add(product);
+    }
+
+    private void addProductWhenProductsHasMaxSize(Product product) {
+        var id = product.id();
+        var price = product.price();
+        var map = productsByIdGrouping.get(id);
+        if (nonNull(map) && !(map.size() == maxNumberOfProductsWithTheSameId && price >= map.lastKey())) {
+            for (var entry : map.entrySet()) {
+                if (entry.getKey() > price) {
+                    if (map.size() == maxNumberOfProductsWithTheSameId) {
+                        var lastEntry = map.pollLastEntry();
+                        products.remove(lastEntry.getValue());
+                    } else {
+                        pollLastProduct();
+                    }
+                    var size = map.size();
+                    map.put(price, product);
+                    if (size < map.size()) {
+                        products.add(product);
+                    }
+                    break;
+                }
+            }
+        } else {
+            pollLastProduct();
+            productsByIdGrouping.put(id, new TreeMap<>(Map.of(price, product)));
+            products.add(product);
+        }
+    }
+
+    private void pollLastProduct() {
+        ofNullable(products.pollLast())
+                .ifPresent(removedProduct -> ofNullable(productsByIdGrouping.get(removedProduct.id()))
+                        .ifPresent(treeMap -> treeMap.remove(removedProduct.price()))
+                );
+    }
+
+    private void handleProduct(Product product) {
+        var price = product.price();
+        var last = products.last();
+        if (price >= last.price()) {
+            addProductToEnd(product);
+            return;
+        }
+        var first = products.first();
+        if (price <= first.price()) {
+            addProductToStart(product);
+        } else {
+            addProduct(product);
+        }
+    }
+
+    private void addProduct(Product product) {
+        var price = product.price();
+        var id = product.id();
+        var map = productsByIdGrouping.get(id);
+        if (nonNull(map) && !(map.size() == maxNumberOfProductsWithTheSameId && price >= map.lastKey())) {
+            for (var entry : map.entrySet()) {
+                if (entry.getKey() > price) {
+                    if (map.size() == maxNumberOfProductsWithTheSameId) {
                         var lastEntry = map.pollLastEntry();
                         products.remove(lastEntry.getValue());
                     }
@@ -123,32 +139,47 @@ public class ProductService {
                     if (size < map.size()) {
                         products.add(product);
                     }
-                } else {
-                    productsByIdGrouping.put(id, new TreeMap<>(Map.of(price, product)));
-                    products.add(product);
-                }
-            } else {
-                var map = productsByIdGrouping.get(id);
-                if (nonNull(map) && !(map.size() == 20 && price >= map.lastKey())) {
-                    for (var entry : map.entrySet()) {
-                        if (entry.getKey() > price) {
-                            if (map.size() == 20) {
-                                var lastEntry = map.pollLastEntry();
-                                products.remove(lastEntry.getValue());
-                            }
-                            var size = map.size();
-                            map.put(price, product);
-                            if (size < map.size()) {
-                                products.add(product);
-                            }
-                            break;
-                        }
-                    }
-                } else {
-                    productsByIdGrouping.put(id, new TreeMap<>(Map.of(price, product)));
-                    products.add(product);
+                    break;
                 }
             }
+        } else {
+            productsByIdGrouping.put(id, new TreeMap<>(Map.of(price, product)));
+            products.add(product);
+        }
+    }
+
+    private void addProductToStart(Product product) {
+        var price = product.price();
+        var id = product.id();
+        var map = productsByIdGrouping.get(id);
+        if (nonNull(map)) {
+            if (map.size() == maxNumberOfProductsWithTheSameId) {
+                var lastEntry = map.pollLastEntry();
+                products.remove(lastEntry.getValue());
+            }
+            var size = map.size();
+            map.put(price, product);
+            if (size < map.size()) {
+                products.add(product);
+            }
+        } else {
+            productsByIdGrouping.put(id, new TreeMap<>(Map.of(price, product)));
+            products.add(product);
+        }
+    }
+
+    private void addProductToEnd(Product product) {
+        var price = product.price();
+        var id = product.id();
+        var map = productsByIdGrouping.get(id);
+        if (nonNull(map)) {
+            if (map.size() < maxNumberOfProductsWithTheSameId) {
+                products.add(product);
+                map.put(price, product);
+            }
+        } else {
+            products.add(product);
+            productsByIdGrouping.put(id, new TreeMap<>(Map.of(price, product)));
         }
     }
 
